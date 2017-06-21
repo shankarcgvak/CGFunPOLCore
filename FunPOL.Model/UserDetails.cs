f23d90d4fc0e02f13a7fb4750a7cc084059a42cf
shankarcgvak <shankar@cgvakindia.com>
2017-06-21 16:44:01 +0530

    Commit I

---
+++ FunPOL.Model/UserDetails.cs
@@ -0,0 +1,35 @@
+﻿using FunPOL.Model.Utilities;
+using System;
+
+namespace FunPOL.Model
+{
+    /// <summary>
+    /// Class User.
+    /// </summary>
+    public class UserDetails : BaseEntity
+    {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the location.
+        /// </summary>
+        /// <value>
+        /// The location.
+        /// </value>
+        public string Location { get; set; }
+
+        /// <summary>
+        /// Gets or sets the email.
+        /// </summary>
+        /// <value>
+        /// The email.
+        /// </value>
+        public string Email { get; set; }
+    }
+}
